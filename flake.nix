c42cef2d20e4c91f7d510485afb384a278abdfe4
will <will.bradshaw50@gmail.com>
2026-02-26 17:37:24 +1100

    feat: add a cheeky flake

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Stable Nixpkgs (use 0.1 for unstable)
+  };
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+    let
+      # The systems supported for this flake's outputs
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper for providing system-specific attributes
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            # Provides a system-specific, configured Nixpkgs
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ ];
+              # Enable using unfree packages
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      # Development environments output by this flake
+
+      # To activate the default environment:
+      # nix develop
+      # Or if you use direnv:
+      # direnv allow
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            nativeBuildInputs = with pkgs; [
+              # Add the flake's formatter to your project's environment
+              self.formatter.${system}
+              bun
+            ];
+
+            # Add any shell logic you want executed when the environment is activated
+            shellHook = "";
+          };
+        }
+      );
+
+      # Nix formatter
+
+      # This applies the formatter that follows RFC 166, which defines a standard format:
+      # https://github.com/NixOS/rfcs/pull/166
+
+      # To format all Nix files:
+      # git ls-files -z '*.nix' | xargs -0 -r nix fmt
+      # To check formatting:
+      # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
+    };
+}
